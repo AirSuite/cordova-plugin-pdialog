25555af40bb8d848314b2727422c9e12485117c4
KleggerKoder <info@air-suite.com>
2017-09-25 13:50:20 -0400

    Crash reports on Play Console #12

--- src/android/io/github/pwlin/cordova/plugins/pdialog/PDialog.java
+++ src/android/io/github/pwlin/cordova/plugins/pdialog/PDialog.java
@@ -189,8 +189,10 @@ public class PDialog extends CordovaPlugin {
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
-				int value = Integer.parseInt(rawArgs);
-				PDialog.pDialogObj.setProgress(value);
+				if (!"[]".equals(rawArgs)) {
+					int value = Integer.parseInt(rawArgs);
+					PDialog.pDialogObj.setProgress(value);
+				}
 			};
 		};
 		this.cordova.getActivity().runOnUiThread(runnable);
